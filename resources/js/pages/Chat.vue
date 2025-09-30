841a3d22654be12c2b8fdf2ca523bbc9e9a87b58
Riley Moura <nmoura49@gmail.com>
2025-09-30 16:24:57 +0100

    Refactor code structure for improved readability and maintainability

--- resources/js/pages/Chat.vue
+++ resources/js/pages/Chat.vue
@@ -4,7 +4,7 @@ import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Welcome">
+    <Head title="Chat">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
